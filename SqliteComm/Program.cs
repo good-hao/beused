137c97c9e389fc271491d5a8002fc085c9eb0e0e
chxue <sh_chxue@c-denkei.cn>
2023-06-30 15:11:01 +0800

    sqlite的操作方式

---
+++ SqliteComm/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.SQLite;
+using System.Data;
+namespace SqliteComm
+{
+    internal class Program
+    {
+        void test()
+        {
+            SqliteHelper db = new SqliteHelper("database.db");
+
+            SQLiteParameter[] parameters = new SQLiteParameter[] {
+    new SQLiteParameter("@Name", "New Value")
+};
+            //"CREATE TABLE IF NOT EXISTS MyTable (Id INTEGER PRIMARY KEY, Name TEXT)"
+
+            int con = db.ExecuteNonQuery("INSERT INTO MyTable (Name) VALUES (@Name)", parameters);
+
+            DataTable table = db.DataTable("SELECT * FROM MyTable WHERE Name=@Name", parameters);
+
+            Console.WriteLine(table);
+        }
+    }
+}
---
+++ SqliteComm/SqliteHelper.cs
@@ -0,0 +1,64 @@
+﻿using System.Data;
+using System.Data.SQLite;
+
+namespace SqliteComm
+{
+    public class SqliteHelper
+    {
+        private readonly string connectionString;
+
+        public SqliteHelper(string databasePath)
+        {
+            connectionString = $"Data Source={databasePath};";
+        }
+
+        public int ExecuteNonQuery(string query, params SQLiteParameter[] parameters)
+        {
+            int count = 0;
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+                using (var command = new SQLiteCommand(query, connection))
+                {
+                    // 添加参数
+                    command.Parameters.AddRange(parameters);
+
+                    // 执行查询
+                    count = command.ExecuteNonQuery();
+                }
+            }
+            return count;
+        }
+
+
+        public DataTable DataTable(string query, params SQLiteParameter[] parameters)
+        {
+            var dataTable = new DataTable();
+
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+                using (var command = new SQLiteCommand(query, connection))
+                {
+                    // 添加参数
+                    command.Parameters.AddRange(parameters);
+
+                    // 执行查询并填充到DataTable中
+                    using (var reader = command.ExecuteReader())
+                    {
+                        dataTable.Load(reader);
+                    }
+                }
+            }
+
+            return dataTable;
+        }
+
+    }
+}
+
+
+
+
