acd694091190382cc9188a5c351475570fc567f9
chxue <sh_chxue@c-denkei.cn>
2023-08-23 16:32:12 +0800

    可以方法

---
+++ myLib/Comm/ExcelHelper.cs
@@ -0,0 +1,182 @@
+﻿using NPOI.SS.UserModel;
+using System.Data;
+
+namespace myLib
+{
+    public class ExcelHelper
+    {
+        /// <summary>
+        /// 将excel文件内容读取到DataTable数据表中
+        /// </summary>
+        /// <param name="fileName">文件完整路径名</param>
+        /// <param name="sheetName">指定读取excel工作薄sheet的名称</param>
+        /// <param name="isFirstRowColumn">第一行是否是DataTable的列名：true=是，false=否</param>
+        /// <returns></returns>
+        public static DataTable ExcelToDataTable(string fileName, string? sheetName = null, bool isFirstRowColumn = true)
+        {
+            DataTable dt = new DataTable();
+            ISheet? sheet = null;//excel工作表
+            int startRow = 0;//数据开始行(排除标题行)
+            FileStream fs;//根据指定路径读取文件
+            IWorkbook? workbook = null;
+            try
+            {
+                if (!File.Exists(fileName))
+                    return dt;
+
+                fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+
+                workbook = WorkbookFactory.Create(fs);
+
+                if (!string.IsNullOrEmpty(sheetName))
+                {
+                    sheet = workbook.GetSheet(sheetName);
+                    //如果没有找到指定的sheetName对应的sheet，则尝试获取第一个sheet
+                    if (sheet == null)
+                    {
+                        sheet = workbook.GetSheetAt(0);
+                    }
+                }
+                else
+                {
+                    //如果没有指定的sheetName，则尝试获取第一个sheet
+                    sheet = workbook.GetSheetAt(0);
+                } 
+
+                if(sheet != null)
+                {
+                    IRow firstRow = sheet.GetRow(0);
+                    int cellCount = firstRow.LastCellNum; //第一行最后一个cell的编号 即总的列数
+                    if (isFirstRowColumn)
+                    {
+                        for (int i = firstRow.FirstCellNum; i < cellCount; ++i)//第一行列数循环
+                        {
+                            ICell cell = firstRow.GetCell(i);
+                            if (cell != null)
+                            {
+                                string cellValue = cell.StringCellValue;
+                                DataColumn column = new DataColumn(cellValue);
+                                dt.Columns.Add(column);
+                            }
+                        }
+                        startRow = sheet.FirstRowNum + 1;
+                    }
+                    else
+                    {
+                        startRow = sheet.FirstRowNum;
+                    }
+
+                    //最后一列的标号
+                    int rowCount = sheet.LastRowNum;
+                    for (int i = startRow; i <= rowCount; ++i)//循环遍历所有行
+                    {
+                        IRow row = sheet.GetRow(i);//第几行
+                        if (row == null)
+                            continue; //没有数据的行默认是null;
+
+                        //将excel表每一行的数据添加到datatable的行中
+                        DataRow dataRow = dt.NewRow();
+                        for (int j = row.FirstCellNum; j < cellCount; ++j)
+                        {
+                            if (row.GetCell(j) != null) //同理，没有数据的单元格都默认是null
+                                dataRow[j] = row.GetCell(j).ToString();
+                        }
+                        dt.Rows.Add(dataRow);
+                    }
+                }
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.Message);
+                return dt;
+            }
+        }
+
+
+        /// <summary>
+        /// 将excel文件流读取到DataTable数据表中
+        /// </summary>
+        /// <param name="fileStream">文件流</param>
+        /// <param name="sheetName">指定读取excel工作薄sheet的名称</param>
+        /// <param name="isFirstRowColumn">第一行是否是DataTable的列名：true=是，false=否</param>
+        /// <returns></returns>
+        public static DataTable ReadStreamToDataTable(Stream fileStream, string? sheetName = null, bool isFirstRowColumn = true)
+        {
+            DataTable dt = new DataTable();
+            ISheet? sheet = null;//excel工作表
+            int startRow = 0;//数据开始行(排除标题行)
+            IWorkbook? workbook = null;
+            try
+            {
+                workbook = WorkbookFactory.Create(fileStream);
+
+                if (!string.IsNullOrEmpty(sheetName))
+                {
+                    sheet = workbook.GetSheet(sheetName);
+                    //如果没有找到指定的sheetName对应的sheet，则尝试获取第一个sheet
+                    if (sheet == null)
+                    {
+                        sheet = workbook.GetSheetAt(0);
+                    }
+                }
+                else
+                {
+                    //如果没有指定的sheetName，则尝试获取第一个sheet
+                    sheet = workbook.GetSheetAt(0);
+                }
+
+                if (sheet != null)
+                {
+                    IRow firstRow = sheet.GetRow(0);
+                    int cellCount = firstRow.LastCellNum; //第一行最后一个cell的编号 即总的列数
+                    if (isFirstRowColumn)
+                    {
+                        for (int i = firstRow.FirstCellNum; i < cellCount; ++i)//第一行列数循环
+                        {
+                            ICell cell = firstRow.GetCell(i);
+                            if (cell != null)
+                            {
+                                string cellValue = cell.StringCellValue;
+                                DataColumn column = new DataColumn(cellValue);
+                                dt.Columns.Add(column);
+                            }
+                        }
+                        startRow = sheet.FirstRowNum + 1;
+                    }
+                    else
+                    {
+                        startRow = sheet.FirstRowNum;
+                    }
+
+                    //最后一列的标号
+                    int rowCount = sheet.LastRowNum;
+                    for (int i = startRow; i <= rowCount; ++i)//循环遍历所有行
+                    {
+                        IRow row = sheet.GetRow(i);//第几行
+                        if (row == null)
+                            continue; //没有数据的行默认是null;
+
+                        //将excel表每一行的数据添加到datatable的行中
+                        DataRow dataRow = dt.NewRow();
+                        for (int j = row.FirstCellNum; j < cellCount; ++j)
+                        {
+                            if (row.GetCell(j) != null) //同理，没有数据的单元格都默认是null
+                                dataRow[j] = row.GetCell(j).ToString();
+                        }
+                        dt.Rows.Add(dataRow);
+                    }
+                }
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.Message);
+                return dt;
+            }
+        }
+
+
+
+    }
+}
